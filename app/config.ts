dd275fda651a262077a4ce8d41722dc6d0772c18
Anish Suman <62011810+anish877@users.noreply.github.com>
2025-04-16 16:48:47 +0530

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "https://draw-express-backend-production.up.railway.app"
-export const WS_URL = "draw-ws-backend-production.up.railway.app"
+export const WS_URL = "https://draw-ws-backend-production.up.railway.app/"
